3a30554c62efbeb2ffa161efb71849b624f02176
JeffreyRivera <jliscue@gmail.com>
2024-06-21 16:36:19 -0500

    SE AGREGO LA FUNCION QUE SE LE PASE LOS CLIENTES A LOS CUALES ESTA ASOCIADO ESE USUARIO

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,12 +21,12 @@ class AuthController extends Controller
             Log::info('Datos de la solicitud de login:', $request->except(['password']));
 
             $request->validate([
-                'name' => 'required|string',
+                'username' => 'required|string',
                 'password' => 'required|string',
             ]);
 
             $credentials = [
-                'name' => htmlspecialchars($request->input('name'), ENT_QUOTES, 'UTF-8'),
+                'name' => htmlspecialchars($request->input('username'), ENT_QUOTES, 'UTF-8'),
                 'password' => $request->input('password')
             ];
 
@@ -37,18 +37,24 @@ class AuthController extends Controller
 
             $user = Auth::user();
             Log::info('Usuario autenticado:', ['user_id' => $user->id, 'name' => $user->name]);
+            //nombre del token
+            $tokenName = 'TOKEN CLIENTE: ' . $user->name;
+            // $abilities = ['*']; // Permitir todas las habilidades
+            $token = $user->createToken($tokenName)->plainTextToken;
 
-            $token = $user->createToken('auth_token')->plainTextToken;
-
+            // Obtener los clientes asociados al usuario
+            $clientes = $user->clientes;
             return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
+                'clientes' => $clientes
             ]);
         } catch (\Exception $e) {
             Log::error('Error en la solicitud de login:', ['error' => $e->getMessage()]);
             return response()->json(['error' => 'Autenticación fallida'], 401);
         }
     }
+
 
     /**
      * Maneja la solicitud de cierre de sesión de un usuario.
--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -14,15 +14,15 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-        Permission::create(['name' => 'ver clientes']);
-        Permission::create(['name' => 'ver roles']);
-        Permission::create(['name' => 'ver usuarios']);
-        Permission::create(['name' => 'asignar permisos']);
-        Permission::create(['name' => 'activar/desactivar clientes']);
-        Permission::create(['name' => 'Crear Usuarios']);
+        Permission::create(['name' => 'ver clientes', 'guard_name' => 'sanctum']);
+        Permission::create(['name' => 'ver roles', 'guard_name' => 'sanctum']);
+        Permission::create(['name' => 'ver usuarios', 'guard_name' => 'sanctum']);
+        Permission::create(['name' => 'asignar permisos', 'guard_name' => 'sanctum']);
+        Permission::create(['name' => 'activar/desactivar clientes', 'guard_name' => 'sanctum']);
+        Permission::create(['name' => 'Crear Usuarios', 'guard_name' => 'sanctum']);
 
         // Crear rol de administrador y asignar permisos
-        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole = Role::create(['name' => 'admin', 'guard_name' => 'sanctum']);
         $adminRole->givePermissionTo(['ver clientes', 'asignar permisos', 'activar/desactivar clientes', 'Crear Usuarios']);
     }
 }
