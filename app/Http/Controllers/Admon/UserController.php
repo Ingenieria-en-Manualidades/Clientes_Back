5243a42c3605bef133eddbef2bf7766bfeec1ca8
JeffreyRivera <jliscue@gmail.com>
2024-06-25 14:42:46 -0500

    cambiossss

--- app/Http/Controllers/Admon/UserController.php
+++ app/Http/Controllers/Admon/UserController.php
@@ -92,7 +92,7 @@ class UserController extends Controller
     public function update(Request $request, int $id)
     {
         $validator = Validator::make($request->all(), [
-            'nombre' => 'required|string|max:15',
+            'nombre' => 'required|string|max:30',
             'rol' => 'required|exists:roles,id',
             'password' => 'nullable|string|min:8|confirmed',
             'clientes' => 'array',
