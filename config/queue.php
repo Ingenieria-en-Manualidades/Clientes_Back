ff858374503e9c0dd3a86d6f9c96e14e3e8f2cc9
DanielDev <auxiliar1.tecnologia-op@ienm.com.co>
2025-10-01 15:35:38 -0500

    bug-fix

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
         'database' => [
             'driver' => 'database',
             'connection' => env('DB_QUEUE_CONNECTION'),
-            'table' => env('DB_QUEUE_TABLE', 'surveys.jobs'),
+            'table' => env('DB_QUEUE_TABLE', 'jobs'),
             'queue' => env('DB_QUEUE', 'default'),
             'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
             'after_commit' => false,
@@ -106,7 +106,7 @@ return [
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'sqlite'),
-        'table' => 'surveys.failed_jobs',
+        'table' => 'failed_jobs',
     ],
 
 ];
