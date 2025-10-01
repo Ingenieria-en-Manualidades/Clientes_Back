bcf0da55bdf9da12372ef9887bbe26bdd8262b19
DanielDev <auxiliar1.tecnologia-op@ienm.com.co>
2025-10-01 15:46:13 -0500

    bug-fix

--- config/rebranding.php
+++ config/rebranding.php
@@ -3,6 +3,6 @@
 return [
     'subject'    => 'IM Ingeniería: nuevo logo + encuesta de satisfacción',
     'survey_url' => env('REBRANDING_SURVEY_URL', 'https://cim.ienm.com.co/encuesta/'),
-    'from_email' => env('MAIL_FROM_ADDRESS', 'no-reply@ienm.com.co'),
+    'from_email' => env('MAIL_FROM_ADDRESS', 'notificaciones@ienm.com.co'),
     'from_name'  => env('MAIL_FROM_NAME', 'IM Ingeniería'),
 ];
